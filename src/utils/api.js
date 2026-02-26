d07fd302c3186874dd39fbdc30202072add7d3b4
shreyashah00 <ysomya519@gmail.com>
2026-02-26 12:36:26 +0545

    fixed the api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
-// const API_URL = "https://intelliquiz-backend-4bha.onrender.com/api";
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://intelliquiz-backend-4bha.onrender.com/api";
+// const API_URL = "http://localhost:5000/api"
 
 const api = axios.create({
   baseURL: API_URL,
